2e3418f4257950f0dddf875ac51727203d9a3fc4
japatel <japatel@paypal.com>
2019-07-01 17:54:45 -0500

    Updates

---
+++ precache-manifest.290de4f2322aaed9ae85b205e84fe3ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8129faf7db4bcfbe83601b3f37fd6b2",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "c378e4e23a65aca28ee2",
+    "url": "/weather/static/css/2.fc5398e4.chunk.css"
+  },
+  {
+    "revision": "135941becaa615e4e97c",
+    "url": "/weather/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c378e4e23a65aca28ee2",
+    "url": "/weather/static/js/2.1416a799.chunk.js"
+  },
+  {
+    "revision": "135941becaa615e4e97c",
+    "url": "/weather/static/js/main.31f6afcc.chunk.js"
+  },
+  {
+    "revision": "3cb3cde5c978189545b4",
+    "url": "/weather/static/js/runtime~main.6cff1e35.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weather/static/media/logo.5d5d9eef.svg"
+  }
+]);
